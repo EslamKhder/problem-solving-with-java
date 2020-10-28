2548dabc1d2972b726afb57a7b1a1da9e3a0210c
EslamKhder <eslamkhder81@gmail.com>
2020-10-28 20:47:00 +0200

    Team

--- src/com/company/GoodNumber.java
+++ src/com/company/GoodNumber.java
@@ -4,16 +4,22 @@ import java.util.Scanner;
 public class GoodNumber {
 
     public static void main(String[] args) {
+        /*
+        10 6
+        1234560 1234560 1234560 1234560
+        1234560 1234560 1234560 1234560
+        1234560 1234560
+        * */
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
         int range = scanner.nextInt();
         int result = 0;
         int counter = 0;
-        for (int i = 0; i < size; i++) { // size = 2
-            String num = scanner.next(); // num =  1
-            for (int x = 0; x < num.length(); x++) { // num.length =  1
+        for (int i = 0; i < size; i++) { // size = 10
+            String num = scanner.next(); // num =  1234560
+            for (int x = 0; x < num.length(); x++) { // num.length =  7
                 counter = 0;
-                for (int z = 0; z <= range; z++) { /// range =  1
+                for (int z = 0; z <= range; z++) { /// range =  0123456
                     if ((num.contains(String.valueOf(z)))) {
                         counter++;
                     }
