be587bd3e35020639404c0a83d1e416a968276d3
EslamKhder <eslamkhder81@gmail.com>
2020-11-08 22:51:31 +0200

    RegularBracketSequence

---
+++ src/com/company/RegularBracketSequence.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class RegularBracketSequence {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        char [] chars = scanner.next().toCharArray();
+        int size = chars.length;
+        int counter = 0;
+        for(int i =0;i<chars.length;i++){
+            if(chars[i] == '('){
+                counter++; // 0
+            } else {
+                counter--;
+                if(counter < 0){
+                    size--;
+                    counter = 0;
+                }
+            }
+        }
+
+        System.out.println(size - counter);
+
+    }
+}
