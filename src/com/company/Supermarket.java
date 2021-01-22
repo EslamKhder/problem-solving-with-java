f6c44098bf4bce7f77f185acb36ca972d75818bc
EslamKhder <eslamkhder81@gmail.com>
2021-01-22 03:52:09 +0200

    Supermarket

---
+++ src/com/company/Supermarket.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Supermarket {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numMarkets = scanner.nextInt();
+        int numKilos = scanner.nextInt();
+        Map<Double,Double> appels = new TreeMap<>();
+        for(int i=0;i<numMarkets;i++){
+            Double price = scanner.nextDouble();
+            Double kilos = scanner.nextDouble();
+            Double result = price / kilos;
+            appels.put(result,result * numKilos);
+        }
+        System.out.println(appels.values().stream().findFirst().get());
+    }
+}
