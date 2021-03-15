7f34889ca71e61d41b3ee796b09b316fd1a8a5f6
EslamKhder <eslamkhder81@gmail.com>
2021-03-16 01:01:19 +0200

    ThreePartsArray

---
+++ src/com/company/ThreePartsArray.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ThreePartsArray {
+    public static void main(String[] args) {
+    }
+}
