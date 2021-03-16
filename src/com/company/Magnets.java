2f6a6bb7e9bee732330b141e9384eabc54f3f05f
EslamKhder <eslamkhder81@gmail.com>
2021-03-16 02:54:54 +0200

    Inbox

--- src/com/company/Magnets.java
+++ src/com/company/Magnets.java
@@ -6,25 +6,6 @@ import java.util.Scanner;
 public class Magnets {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int numMagnets = scanner.nextInt();
-        String temp = "";
-        int counter = 0;
-        String num = null;
-        if(numMagnets == 1){
-            System.out.println(1);
-            return;
-        }
-        if(numMagnets == 2){
-            System.out.println(2);
-            return;
-        }
-        for (int i=0;i<numMagnets;i++) {
-            num = scanner.next();
-            if(!temp.isEmpty() && temp.toCharArray()[1] != num.toCharArray()[0]){
-                counter++;
-            }
-            temp = num;
-        }
-        System.out.println(counter);
+
     }
 }
