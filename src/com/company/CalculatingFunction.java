fcc36b2a9e63b174d549b999c262035e172e5f77
EslamKhder <eslamkhder81@gmail.com>
2021-04-30 23:37:55 +0200

    DifferentIsGood

--- src/com/company/CalculatingFunction.java
+++ src/com/company/CalculatingFunction.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class CalculatingFunction {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        long num = scanner.nextLong();
-        if (num % 2 == 0) {
-            System.out.println(num / 2);
+        long number = scanner.nextLong();
+        if(number %2 ==0){
+            System.out.println(number/2);
         } else {
-            System.out.println(-(num+1)/2);
+            System.out.println(-((number/2) + 1));
         }
     }
 }
